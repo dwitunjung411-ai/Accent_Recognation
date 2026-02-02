45ca8489810f99fd981324f9de8abbf5b1526f1b
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-03 01:50:57 +0700

    Update stream_app_py.py

--- stream_app_py.py
+++ stream_app_py.py
@@ -22,8 +22,7 @@ N_FFT = 2048
 HOP_LENGTH = 512
 
 # Nama file yang diharapkan (sesuaikan jika berbeda)
-EMBEDDING_MODEL_KERAS = "embedding_model.keras"     # prefer: model utuh
-EMBEDDING_WEIGHTS_H5 = "embedding_weights.h5"       # alternatif: hanya weights
+EMBEDDING_MODELS = "embedding_model"     # prefer: model utuh
 PREPROCESS_FILE = "preprocess.joblib"               # scaler_usia + ohe (gender, provinsi)
 
 # Batas UI agar tidak terlalu berat di Streamlit Cloud
