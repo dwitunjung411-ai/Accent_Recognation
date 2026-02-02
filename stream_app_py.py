325833dbb2d3d4b0f7dc395f8e4603e24ff125d7
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-03 01:04:05 +0700

    Update stream_app_py.py

--- stream_app_py.py
+++ stream_app_py.py
@@ -58,15 +58,9 @@ def create_and_save_dummy_model():
     
     # Bangun model sesuai skripsi
     model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', 
-                               input_shape=input_shape),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(256, activation='relu'),
         tf.keras.layers.Dropout(0.3),
         tf.keras.layers.Dense(128, activation='relu')
     ])
