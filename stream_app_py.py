e8c31daf3d9358f34669092b7c649ffb4805ae9b
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-04 00:30:42 +0700

    Update stream_app_py.pyemot

--- stream_app_py.py
+++ stream_app_py.py
@@ -96,11 +96,11 @@ def main():
                         # Tampilkan metadata yang terkait dengan audio
                         st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-calendar'></i> 📅Usia: {usia}</h2>", unsafe_allow_html=True)
                         st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-venus-mars'></i> 🗣️Gender: {gender}</h2>", unsafe_allow_html=True)
-                        st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-map-marker-alt'></i> 🌍Provinsi: {provinsi}</h2>", unsafe_allow_html=True)
+                        st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-map-marker-alt'></i> 📍Provinsi: {provinsi}</h2>", unsafe_allow_html=True)
                         
                         # Prediksi aksen dari audio yang di-upload
                         aksen = predict_accent(tmp_path)
-                        st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-volume-up'></i> **Prediksi Aksen:** {aksen}</h2>", unsafe_allow_html=True)
+                        st.markdown(f"<h2 style='color:#FF6347;'><i class='fas fa-volume-up'></i> 🎭Prediksi Aksen: {aksen}</h2>", unsafe_allow_html=True)
                     else:
                         st.write("Metadata tidak ditemukan untuk audio ini.")
                 
