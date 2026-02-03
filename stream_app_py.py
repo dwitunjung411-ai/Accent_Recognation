70c51bfbec9b1e8b1a11eaabb39ad1448b629178
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-04 02:37:48 +0700

    Update stream_app_py.py model

--- stream_app_py.py
+++ stream_app_py.py
@@ -13,7 +13,8 @@ def load_metadata(csv_path):
 # Fungsi untuk memproses file audio dan memprediksi aksen
 def predict_accent(audio_path):
     # Memuat model aksen
-    model = 'model_aksen.h5'
+    model = tf.keras.models.load_model("model_aksen.keras")
+        return model
     
     # Ekstraksi fitur audio (MFCC)
     y, sr = librosa.load(audio_path, sr=None)
