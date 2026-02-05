8aa97e0e7ed7ff1047d4e2d35f8d319038b99445
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-06 01:26:18 +0700

    Update stream_detect_app_py.py

--- stream_detect_app_py.py
+++ stream_detect_app_py.py
@@ -28,7 +28,7 @@ class PrototypicalNetwork(tf.keras.Model):
 # ==========================================================
 @st.cache_resource
 def load_accent_model():
-    model_path = "model_aksen.keras"
+    model = tf.keras.models.load_model("model_aksen (1).keras", custom_objects=custom_objects)
     if os.path.exists(model_path):
         # Menyertakan custom_objects agar PrototypicalNetwork dikenali
         custom_objects = {"PrototypicalNetwork": PrototypicalNetwork}
